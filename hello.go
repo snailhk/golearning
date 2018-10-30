46e109bfacef9be12fc4093b636692f850c7acbc
xiezehua <xiezehua@ainirobot.com>
2018-10-30 18:47:55 +0800

    [ADD  var learn

--- hello.go
+++ hello.go
@@ -2,10 +2,28 @@ package main
 
 import "fmt"
 
-func variable(){
+func variableZeroValue() {
 	var a int
 	var s string
+	fmt.Println(a, s)
 }
-func main(){
+
+func variableInitialValue() {
+	var a, b int = 1, 2
+	var s string = "abc"
+	fmt.Println(a, b, s)
+}
+
+func variableTypeDeduction() {
+	var a, b, c, s = 3, 4, true, "def"
+	e, f := false, "sss"
+	fmt.Println(a, b, c, s)
+	fmt.Println(e, f)
+}
+
+func main() {
 	fmt.Println("Hello world")
+	variableZeroValue()
+	variableInitialValue()
+	variableTypeDeduction()
 }
